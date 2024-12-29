f6d67f2075c41aab5df76b1c4352f08c4668bd4e
madiha-ahmed-chowdhury <madihaahmedchowdhury6183@gmail.com>
2024-12-29 21:53:22 +0600

    Static data to requests

--- niramoy/app/src/main/java/com/example/niramoy/user/ui/home/HomeFragment.kt
+++ niramoy/app/src/main/java/com/example/niramoy/user/ui/home/HomeFragment.kt
@@ -4,39 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.niramoy.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var requestAdapter: RequestAdapter
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private val acceptedRequests = listOf(
+        AcceptedRequest("Postpartum Care", "Nurse Nipa"),
+        AcceptedRequest("Jaundice Routine Care", "Nurse Rahma"),
+        //("Routine Check-up", "Nurse Clara")
+    )
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout using ViewBinding
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        // Set up the RecyclerView with the adapter and layout manager
+        requestAdapter = RequestAdapter(acceptedRequests)
+        binding.rvAcceptedRequests.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvAcceptedRequests.adapter = requestAdapter
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return binding.root
     }
 }
